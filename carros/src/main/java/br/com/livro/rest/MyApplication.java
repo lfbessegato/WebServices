f9917674074a86fdd138b333561e38e1c1355e44
Lfbessegato <Lfrbessegato@gmail.com>
2021-06-07 16:36:32 -0300

    Capitulo 10 - Upload

--- carros/src/main/java/br/com/livro/rest/MyApplication.java
+++ carros/src/main/java/br/com/livro/rest/MyApplication.java
@@ -1,25 +1,30 @@
 package br.com.livro.rest;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+
 public class MyApplication extends Application {
-//	@Override
-//	public Set<Object> getSingletons() {
-//		Set<Object> singletons = new HashSet<>();
-//		// Driver do Jettison para gerar JSON.
-//		singletons.add(new JettisonFeature());
-//		return singletons;
-//	}
+	@Override
+	public Set<Object> getSingletons() {
+		Set<Object> singletons = new HashSet<>();
+		//Driver do Jettison para gerar JSON.
+		
+		// Suporte ao file upload
+		singletons.add(new MultiPartFeature());
+		return singletons;
+	}
 
 	@Override
 	public Map<String, Object> getProperties() {
 		Map<String, Object> properties = new HashMap<>();
 		// Configura o pacote para fazer scan das classes com anotações REST.
-		properties
-				.put("jersey.config.server.provider.packages", "br.com.livro");
+		properties.put("jersey.config.server.provider.packages", "br.com.livro");
 		return properties;
 	}
 }
